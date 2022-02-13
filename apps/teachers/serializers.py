1b9230abc4a6bbc4e0e0a607eb0ca71abb015bb1
harshithaputtaswamy <44926484+harshithaputtaswamy@users.noreply.github.com>
2022-02-14 01:25:10 +0530

    Update serializers.py

--- apps/teachers/serializers.py
+++ apps/teachers/serializers.py
@@ -16,23 +16,26 @@ class TeacherAssignmentSerializer(serializers.ModelSerializer):
         return Assignment.objects.create(**validated_data)
 
     def validate(self, attrs):
-        
-        if 'content' in attrs and attrs['content']:
-            raise serializers.ValidationError('Teacher cannot change the content of the assignment')
-        
-        if self.initial_data['assignment'].state == 'DRAFT':
-            raise serializers.ValidationError('SUBMITTED assignments can only be graded')
-        if self.initial_data['assignment'].state == 'GRADED':
-            raise serializers.ValidationError('GRADED assignments cannot be graded again')
-        if self.initial_data['assignment'].state == 'SUBMITTED':
-            self.initial_data['assignment'].state = 'GRADED'
-        if self.initial_data['assignment'].teacher.id != self.initial_data['teacher']:
-            raise serializers.ValidationError('Teacher cannot grade for other teachers assignment')
         if 'grade' in attrs and attrs['grade'] not in ["A", "B", "C", "D"]:
             raise serializers.ValidationError('Invalid Grade')
                 
+        if 'content' in attrs and attrs['content']:
+            raise serializers.ValidationError('Teacher cannot change the content of the assignment')
+       
+        if self.initial_data['assignment'].teacher.id != self.initial_data['teacher']:
+            raise serializers.ValidationError('Teacher cannot grade for other teachers assignment')
+        
         if 'student' in attrs:
             raise serializers.ValidationError('Teacher cannot change the student who submitted the assignment')
+
+        if self.initial_data['assignment'].state == 'DRAFT':
+            raise serializers.ValidationError('SUBMITTED assignments can only be graded')
+
+        if self.initial_data['assignment'].state == 'GRADED':
+            raise serializers.ValidationError('GRADED assignments cannot be graded again')
+
+        if self.initial_data['assignment'].state == 'SUBMITTED':
+            self.initial_data['assignment'].state = 'GRADED'
 
         if self.partial:
             return attrs
